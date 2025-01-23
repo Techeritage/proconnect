7b066be5777de029a64f4163bee385c60fe8785d
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2025-01-23 18:13:28 +0100

    update

--- components/sections/HiringSteps.jsx
+++ components/sections/HiringSteps.jsx
@@ -12,10 +12,10 @@ const HiringSteps = () => {
         connect with top candidates effortlessly.
       </p>
 
-      <div className="md:relative max-md:grid gap-y-10 md:bg-wavy wavy md:max-w-4xl mx-auto bg-contain mt-28 md:min-w-max md:min-h-[300px]">
+      <div className="md:relative max-md:grid gap-y-20 md:gap-y-10 md:bg-wavy wavy md:max-w-4xl mx-auto bg-contain mt-16 md:mt-28 md:min-w-max md:min-h-[300px]">
         <div className="md:absolute -top-7 -left-[120px] myFlex flex-col md:max-w-[300px] justify-center gap-3">
           <div className="bg-primary rounded-full size-16 flex-center">
-            <h2 className="font-aeoReg text-white">1</h2>
+            <h2 className="font-aeoBold text-white">1</h2>
           </div>
           <p className="text-base text-center md:text-lg font-aeoBold max-w-[80%]">
             Submit Your Requirements
@@ -28,7 +28,7 @@ const HiringSteps = () => {
 
         <div className="md:absolute top-4 left-[50%] md:max-w-[300px] md:-translate-x-[50%] myFlex flex-col justify-center gap-3">
           <div className="bg-primary rounded-full size-16 flex-center">
-            <h2 className="font-aeoReg text-white">2</h2>
+            <h2 className="font-aeoBold text-white">2</h2>
           </div>
           <p className="text-base md:text-lg font-aeoBold max-w-[80%] text-center">
             Candidate Shortlisting
@@ -41,7 +41,7 @@ const HiringSteps = () => {
 
         <div className="md:absolute -right-[120px] -top-7 md:max-w-[300px] myFlex flex-col justify-center gap-3">
           <div className="bg-primary rounded-full size-16 flex-center">
-            <h2 className="font-aeoReg text-white">3</h2>
+            <h2 className="font-aeoBold text-white">3</h2>
           </div>
           <p className="text-base md:text-lg font-aeoBold max-w-[80%] text-center">
             Schedule Interviews
