ff38bfa0d635042823be0d96ae6b58b216d91b78
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2025-01-23 20:22:50 +0100

    update

--- components/sections/Testimonials.jsx
+++ components/sections/Testimonials.jsx
@@ -1,11 +1,174 @@
-import React from 'react'
+import { Star } from "lucide-react";
 
 const Testimonials = () => {
   return (
-    <div>
-      Testimonials
-    </div>
-  )
-}
+    <section className="myContainer">
+      <h1>
+        What Our <br /> Clients Say
+      </h1>
+      <p className="opacity-85 mt-2 text-sm w-[90%]">
+        Discover the experiences of employers and job seekers{" "}
+        <br className="max-md:hidden" /> who have successfully connected through
+        ProConnect.
+      </p>
+      <div className="mt-10 myFlex gap-4 overflow-x-scroll no-scrollbar">
+        <div className="bg-bgGray space-y-5 rounded-2xl p-3 md:p-5 pb-16 max-w-[330px] min-w-[330px] md:min-w-[400px] md:max-w-[450px]">
+          <div className="myFlex justify-between">
+            <div className="myFlex gap-2">
+              <div className="size-14 rounded-full bg-gray-200" />
+              <div>
+                <p className="font-aeoBold text-sm">Emmanuel Adewale</p>
+                <p className="opacity-85 text-sm">Recruiter</p>
+              </div>
+            </div>
+            <div className="myFlex">
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+            </div>
+          </div>
+          <p>
+            ProConnect helped us find the perfect candidates quickly and
+            efficiently. The process was seamless and stress-free!
+          </p>
+        </div>
+        <div className="bg-bgGray space-y-5 rounded-2xl p-3 md:p-5 pb-16 max-w-[330px] min-w-[330px] md:min-w-[400px] md:max-w-[450px]">
+          <div className="myFlex justify-between">
+            <div className="myFlex gap-2">
+              <div className="size-14 rounded-full bg-gray-200" />
+              <div>
+                <p className="font-aeoBold text-sm">Emmanuel Adewale</p>
+                <p className="opacity-85 text-sm">Recruiter</p>
+              </div>
+            </div>
+            <div className="myFlex">
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+            </div>
+          </div>
+          <p>
+            ProConnect helped us find the perfect candidates quickly and
+            efficiently. The process was seamless and stress-free!
+          </p>
+        </div>
+        <div className="bg-bgGray space-y-5 rounded-2xl p-3 md:p-5 pb-16 max-w-[330px] min-w-[330px] md:min-w-[400px] md:max-w-[450px]">
+          <div className="myFlex justify-between">
+            <div className="myFlex gap-2">
+              <div className="size-14 rounded-full bg-gray-200" />
+              <div>
+                <p className="font-aeoBold text-sm">Emmanuel Adewale</p>
+                <p className="opacity-85 text-sm">Recruiter</p>
+              </div>
+            </div>
+            <div className="myFlex">
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+            </div>
+          </div>
+          <p>
+            ProConnect helped us find the perfect candidates quickly and
+            efficiently. The process was seamless and stress-free!
+          </p>
+        </div>
+        <div className="bg-bgGray space-y-5 rounded-2xl p-3 md:p-5 pb-16 max-w-[330px] min-w-[330px] md:min-w-[400px] md:max-w-[450px]">
+          <div className="myFlex justify-between">
+            <div className="myFlex gap-2">
+              <div className="size-14 rounded-full bg-gray-200" />
+              <div>
+                <p className="font-aeoBold text-sm">Emmanuel Adewale</p>
+                <p className="opacity-85 text-sm">Recruiter</p>
+              </div>
+            </div>
+            <div className="myFlex">
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+            </div>
+          </div>
+          <p>
+            ProConnect helped us find the perfect candidates quickly and
+            efficiently. The process was seamless and stress-free!
+          </p>
+        </div>
+        <div className="bg-bgGray space-y-5 rounded-2xl p-3 md:p-5 pb-16 max-w-[330px] min-w-[330px] md:min-w-[400px] md:max-w-[450px]">
+          <div className="myFlex justify-between">
+            <div className="myFlex gap-2">
+              <div className="size-14 rounded-full bg-gray-200" />
+              <div>
+                <p className="font-aeoBold text-sm">Emmanuel Adewale</p>
+                <p className="opacity-85 text-sm">Recruiter</p>
+              </div>
+            </div>
+            <div className="myFlex">
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+            </div>
+          </div>
+          <p>
+            ProConnect helped us find the perfect candidates quickly and
+            efficiently. The process was seamless and stress-free!
+          </p>
+        </div>
+        <div className="bg-bgGray space-y-5 rounded-2xl p-3 md:p-5 pb-16 max-w-[330px] min-w-[330px] md:min-w-[400px] md:max-w-[450px]">
+          <div className="myFlex justify-between">
+            <div className="myFlex gap-2">
+              <div className="size-14 rounded-full bg-gray-200" />
+              <div>
+                <p className="font-aeoBold text-sm">Emmanuel Adewale</p>
+                <p className="opacity-85 text-sm">Recruiter</p>
+              </div>
+            </div>
+            <div className="myFlex">
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+            </div>
+          </div>
+          <p>
+            ProConnect helped us find the perfect candidates quickly and
+            efficiently. The process was seamless and stress-free!
+          </p>
+        </div>
+        <div className="bg-bgGray space-y-5 rounded-2xl p-3 md:p-5 pb-16 max-w-[330px] min-w-[330px] md:min-w-[400px] md:max-w-[450px]">
+          <div className="myFlex justify-between">
+            <div className="myFlex gap-2">
+              <div className="size-14 rounded-full bg-gray-200" />
+              <div>
+                <p className="font-aeoBold text-sm">Emmanuel Adewale</p>
+                <p className="opacity-85 text-sm">Recruiter</p>
+              </div>
+            </div>
+            <div className="myFlex">
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+              <Star size={16} color="#FFC107" fill="#FFC107" />
+            </div>
+          </div>
+          <p>
+            ProConnect helped us find the perfect candidates quickly and
+            efficiently. The process was seamless and stress-free!
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
