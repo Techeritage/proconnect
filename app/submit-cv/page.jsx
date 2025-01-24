334e82b36dbeaccf30f02d4a8e072da99a470fdb
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2025-01-24 02:06:09 +0100

    add submit cv page

---
+++ app/submit-cv/page.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const SubmitCVPage = () => {
+  return (
+    <main className="min-h-dvh !pt-20">
+      <section className="grid gap-3 md:gap-7 myContainer">
+        <h1 className="">
+          Looking for your <br /> next opportunity?
+        </h1>
+        <p className="md:max-w-[80%] md:leading-[30px] font-aeoReg opacity-85">
+          Submit your CV and let top employers discover your talent.{" "}
+          <br className="max-md:hidden" /> Fill out the form below to get
+          started!"
+        </p>
+      </section>
+      {/**form */}
+    </main>
+  );
+};
+
+export default SubmitCVPage;
