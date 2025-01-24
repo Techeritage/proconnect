24fd400c65e29b845c2bb0d44c8dacf83b893712
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2025-01-24 09:59:12 +0100

    add blog page

---
+++ app/blog/page.jsx
@@ -0,0 +1,119 @@
+import { Clock } from "lucide-react";
+
+const BlogPage = () => {
+  return (
+    <main className="!pt-20 min-h-dvh">
+      <section className="grid gap-3 md:gap-7 myContainer">
+        <h1 className="">Insights & Resources</h1>
+        <p className="md:max-w-[80%] md:leading-[30px] font-aeoReg opacity-85">
+          Stay informed with the latest career advice, industry <br /> trends,
+          and hiring insights.
+        </p>
+      </section>
+      <section className="myContainer !pt-0 grid md:grid-cols-2 gap-x-4 gap-y-7 lg:grid-cols-3 max-w-[1440px] mx-auto">
+        <div className="border border-gray-300 rounded-2xl p-2">
+          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
+          <h4 className="text-base md:text-lg font-aeoBold mt-2">
+            5 Proven Strategies to Attract Top Talent in 2024
+          </h4>
+          <p className="line-clamp-2 mt-2">
+            Discover actionable tips to stand out in a competitive market and
+            secure the best candidates for your team.
+          </p>
+          <div className="myFlex gap-1 opacity-85 mt-4">
+            <Clock size={18} />
+            <p className="text-sm">3 mins reading time</p>
+          </div>
+        </div>
+        <div className="border border-gray-300 rounded-2xl p-2">
+          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
+          <h4 className="text-base md:text-lg font-aeoBold mt-2">
+            5 Proven Strategies to Attract Top Talent in 2024
+          </h4>
+          <p className="line-clamp-2 mt-2">
+            Discover actionable tips to stand out in a competitive market and
+            secure the best candidates for your team.
+          </p>
+          <div className="myFlex gap-1 opacity-85 mt-4">
+            <Clock size={18} />
+            <p className="text-sm">3 mins reading time</p>
+          </div>
+        </div>
+        <div className="border border-gray-300 rounded-2xl p-2">
+          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
+          <h4 className="text-base md:text-lg font-aeoBold mt-2">
+            5 Proven Strategies to Attract Top Talent in 2024
+          </h4>
+          <p className="line-clamp-2 mt-2">
+            Discover actionable tips to stand out in a competitive market and
+            secure the best candidates for your team.
+          </p>
+          <div className="myFlex gap-1 opacity-85 mt-4">
+            <Clock size={18} />
+            <p className="text-sm">3 mins reading time</p>
+          </div>
+        </div>
+        <div className="border border-gray-300 rounded-2xl p-2">
+          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
+          <h4 className="text-base md:text-lg font-aeoBold mt-2">
+            5 Proven Strategies to Attract Top Talent in 2024
+          </h4>
+          <p className="line-clamp-2 mt-2">
+            Discover actionable tips to stand out in a competitive market and
+            secure the best candidates for your team.
+          </p>
+          <div className="myFlex gap-1 opacity-85 mt-4">
+            <Clock size={18} />
+            <p className="text-sm">3 mins reading time</p>
+          </div>
+        </div>
+        <div className="border border-gray-300 rounded-2xl p-2">
+          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
+          <h4 className="text-base md:text-lg font-aeoBold mt-2">
+            5 Proven Strategies to Attract Top Talent in 2024
+          </h4>
+          <p className="line-clamp-2 mt-2">
+            Discover actionable tips to stand out in a competitive market and
+            secure the best candidates for your team.
+          </p>
+          <div className="myFlex gap-1 opacity-85 mt-4">
+            <Clock size={18} />
+            <p className="text-sm">3 mins reading time</p>
+          </div>
+        </div>
+        <div className="border border-gray-300 rounded-2xl p-2">
+          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
+          <h4 className="text-base md:text-lg font-aeoBold mt-2">
+            5 Proven Strategies to Attract Top Talent in 2024
+          </h4>
+          <p className="line-clamp-2 mt-2">
+            Discover actionable tips to stand out in a competitive market and
+            secure the best candidates for your team.
+          </p>
+          <div className="myFlex gap-1 opacity-85 mt-4">
+            <Clock size={18} />
+            <p className="text-sm">3 mins reading time</p>
+          </div>
+        </div>
+        <div className="border border-gray-300 rounded-2xl p-2">
+          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
+          <h4 className="text-base md:text-lg font-aeoBold mt-2">
+            5 Proven Strategies to Attract Top Talent in 2024
+          </h4>
+          <p className="line-clamp-2 mt-2">
+            Discover actionable tips to stand out in a competitive market and
+            secure the best candidates for your team.
+          </p>
+          <div className="myFlex gap-1 opacity-85 mt-4">
+            <Clock size={18} />
+            <p className="text-sm">3 mins reading time</p>
+          </div>
+        </div>
+      </section>
+      <p className="text-center font-aeoReg">Load More</p>
+      {/**Subscribe to newsletter */}
+    </main>
+  );
+};
+
+export default BlogPage;
