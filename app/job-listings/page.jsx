64f40d334e262dd7bf0620cd7c30f24ac023a9d4
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2025-01-25 12:34:16 +0100

    update

--- app/job-listings/page.jsx
+++ app/job-listings/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const JobListingsPage = () => {
   return (
-    <main>
+    <main className='min-h-dvh !pt-24'>
+      <section className='bg-[#01184f] h-[200px] w-full'>
+
+      </section>
       {/* faq */}
     </main>
   )
