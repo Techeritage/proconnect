ac00d42080bea20ab7b7a3bcdda4bfc30a6aa78e
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2025-01-25 22:14:36 +0100

    add blog post page

---
+++ app/blog/[id]/page.jsx
@@ -0,0 +1,14 @@
+const BlogPostPage = ({ params }) => {
+  const { id } = params;
+  const decodedId = decodeURI(id);
+  return (
+    <main className="min-h-dvh !pt-24">
+      <section>
+        <h1 className="text-center max-w-[90%] md:max-w-[60%] mx-auto py-16 md:py-20">{decodedId}</h1>
+        <div className="w-full h-[300px] bg-gray-200" />
+      </section>
+    </main>
+  );
+};
+
+export default BlogPostPage;
--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,5 +1,6 @@
 import CTA from "@/components/sections/CTA";
 import { Clock } from "lucide-react";
+import Link from "next/link";
 
 const BlogPage = () => {
   return (
@@ -12,104 +13,24 @@ const BlogPage = () => {
         </p>
       </section>
       <section className="myContainer !pt-0 grid md:grid-cols-2 gap-x-4 gap-y-7 lg:grid-cols-3 max-w-[1440px] mx-auto">
-        <div className="border border-gray-300 rounded-2xl p-2">
-          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
-          <h4 className="text-base md:text-lg font-aeoBold mt-2">
-            5 Proven Strategies to Attract Top Talent in 2024
-          </h4>
-          <p className="line-clamp-2 mt-2">
-            Discover actionable tips to stand out in a competitive market and
-            secure the best candidates for your team.
-          </p>
-          <div className="myFlex gap-1 opacity-85 mt-4">
-            <Clock size={18} />
-            <p className="text-sm">3 mins reading time</p>
+        {Array.from({ length: 6 }, (_, i) => (
+          <div key={i} className="border border-gray-300 rounded-2xl p-2">
+            <Link href={`/blog/${"5 Proven Strategies to Attract Top Talent in 2024"}`}>
+              <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
+              <h4 className="text-base md:text-lg font-aeoBold mt-2">
+                5 Proven Strategies to Attract Top Talent in 2024
+              </h4>
+              <p className="line-clamp-2 mt-2">
+                Discover actionable tips to stand out in a competitive market
+                and secure the best candidates for your team.
+              </p>
+              <div className="myFlex gap-1 opacity-85 mt-4">
+                <Clock size={18} />
+                <p className="text-sm">3 mins reading time</p>
+              </div>
+            </Link>
           </div>
-        </div>
-        <div className="border border-gray-300 rounded-2xl p-2">
-          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
-          <h4 className="text-base md:text-lg font-aeoBold mt-2">
-            5 Proven Strategies to Attract Top Talent in 2024
-          </h4>
-          <p className="line-clamp-2 mt-2">
-            Discover actionable tips to stand out in a competitive market and
-            secure the best candidates for your team.
-          </p>
-          <div className="myFlex gap-1 opacity-85 mt-4">
-            <Clock size={18} />
-            <p className="text-sm">3 mins reading time</p>
-          </div>
-        </div>
-        <div className="border border-gray-300 rounded-2xl p-2">
-          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
-          <h4 className="text-base md:text-lg font-aeoBold mt-2">
-            5 Proven Strategies to Attract Top Talent in 2024
-          </h4>
-          <p className="line-clamp-2 mt-2">
-            Discover actionable tips to stand out in a competitive market and
-            secure the best candidates for your team.
-          </p>
-          <div className="myFlex gap-1 opacity-85 mt-4">
-            <Clock size={18} />
-            <p className="text-sm">3 mins reading time</p>
-          </div>
-        </div>
-        <div className="border border-gray-300 rounded-2xl p-2">
-          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
-          <h4 className="text-base md:text-lg font-aeoBold mt-2">
-            5 Proven Strategies to Attract Top Talent in 2024
-          </h4>
-          <p className="line-clamp-2 mt-2">
-            Discover actionable tips to stand out in a competitive market and
-            secure the best candidates for your team.
-          </p>
-          <div className="myFlex gap-1 opacity-85 mt-4">
-            <Clock size={18} />
-            <p className="text-sm">3 mins reading time</p>
-          </div>
-        </div>
-        <div className="border border-gray-300 rounded-2xl p-2">
-          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
-          <h4 className="text-base md:text-lg font-aeoBold mt-2">
-            5 Proven Strategies to Attract Top Talent in 2024
-          </h4>
-          <p className="line-clamp-2 mt-2">
-            Discover actionable tips to stand out in a competitive market and
-            secure the best candidates for your team.
-          </p>
-          <div className="myFlex gap-1 opacity-85 mt-4">
-            <Clock size={18} />
-            <p className="text-sm">3 mins reading time</p>
-          </div>
-        </div>
-        <div className="border border-gray-300 rounded-2xl p-2">
-          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
-          <h4 className="text-base md:text-lg font-aeoBold mt-2">
-            5 Proven Strategies to Attract Top Talent in 2024
-          </h4>
-          <p className="line-clamp-2 mt-2">
-            Discover actionable tips to stand out in a competitive market and
-            secure the best candidates for your team.
-          </p>
-          <div className="myFlex gap-1 opacity-85 mt-4">
-            <Clock size={18} />
-            <p className="text-sm">3 mins reading time</p>
-          </div>
-        </div>
-        <div className="border border-gray-300 rounded-2xl p-2">
-          <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
-          <h4 className="text-base md:text-lg font-aeoBold mt-2">
-            5 Proven Strategies to Attract Top Talent in 2024
-          </h4>
-          <p className="line-clamp-2 mt-2">
-            Discover actionable tips to stand out in a competitive market and
-            secure the best candidates for your team.
-          </p>
-          <div className="myFlex gap-1 opacity-85 mt-4">
-            <Clock size={18} />
-            <p className="text-sm">3 mins reading time</p>
-          </div>
-        </div>
+        ))}
       </section>
       <p className="text-center font-aeoReg mt-7 mb-20 underline">Load More</p>
       <CTA
