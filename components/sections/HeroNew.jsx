4b0b06f3f31dce9df178e724da09aec00eeb49ac
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2025-02-02 15:39:41 +0100

    fix

--- components/sections/HeroNew.jsx
+++ components/sections/HeroNew.jsx
@@ -1,29 +1,52 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Button from "../Button";
 
 const HeroNew = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleVideoLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
-    <section className="relative min-h-screen overflow-x-hidden">
+    <section className="relative min-h-screen overflow-hidden">
+      {/* Desktop Video */}
       <video
         autoPlay
         src="/hero.mp4"
         loop
         muted
-        playsInline // Ensures proper playback on mobile devices
-        className="absolute max-md:hidden inset-0 w-full h-full object-cover md:object-center aspect-video"
+        onLoadedData={handleVideoLoad}
+        playsInline
+        preload="auto"
+        className="absolute max-md:hidden inset-0 w-full h-full object-cover object-center"
       />
+
+      {/* Mobile Video */}
       <video
         autoPlay
         src="/hero3.mp4"
         loop
         muted
-        playsInline // Ensures proper playback on mobile devices
-        className="absolute md:hidden inset-0 w-full h-full object-cover object-[70%_center] aspect-video"
+        onLoadedData={handleVideoLoad}
+        playsInline
+        preload="auto"
+        className="absolute md:hidden inset-0 w-full h-full object-cover object-[70%_center]"
       />
-      <div className="absolute myFlex flex-col gap-7 md:gap-8 justify-center text-white z-40 top-0 bg-black/60 left-0 right-0 bottom-0">
-        <h1 className="text-center">
-          Empowering Talent. <br />{" "}
-          <span className="">Building Exceptional the Teams.</span>
+
+      {/* Fallback Loading Indicator */}
+      {isLoading && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black text-white z-30">
+          <p>Loading video...</p>
+        </div>
+      )}
+
+      {/* Content Overlay */}
+      <div className="absolute inset-0 flex flex-col items-center justify-center gap-7 md:gap-8 text-white bg-black/60 z-40 px-4">
+        <h1 className="text-center font-medium">
+          Empowering Talent. <br />
+          <span>Building Exceptional Teams.</span>
         </h1>
         <p className="opacity-80 max-w-[90%] lg:max-w-[60%] mx-auto text-center md:text-lg">
           At ProConnect, we don’t just connect you with top talent—we help
