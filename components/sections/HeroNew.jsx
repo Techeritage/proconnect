c2e9d6bbfcae5019014a544447afef82dc3f8df5
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2025-02-02 15:26:27 +0100

    update

--- components/sections/HeroNew.jsx
+++ components/sections/HeroNew.jsx
@@ -10,7 +10,15 @@ const HeroNew = () => {
         loop
         muted
         playsInline // Ensures proper playback on mobile devices
-        className="absolute inset-0 w-full h-full object-cover md:object-center aspect-video"
+        className="absolute max-md:hidden inset-0 w-full h-full object-cover md:object-center aspect-video"
+      />
+      <video
+        autoPlay
+        src="/hero3.mp4"
+        loop
+        muted
+        playsInline // Ensures proper playback on mobile devices
+        className="absolute md:hidden inset-0 w-full h-full object-cover object-[70%_center] aspect-video"
       />
       <div className="absolute myFlex flex-col gap-7 md:gap-8 justify-center text-white z-40 top-0 bg-black/60 left-0 right-0 bottom-0">
         <h1 className="text-center">
