6e7548ae259c8d6513e81c00d612b36ade8a797d
Fatomoye Emmanuel <fatomoyeemmanuel1@gmail.com>
2025-02-09 15:48:09 +0100

    upsate

--- app/(main)/blog/page.jsx
+++ app/(main)/blog/page.jsx
@@ -1,8 +1,26 @@
+"use client";
+
 import CTA from "@/components/sections/CTA";
+import { useBlogs } from "@/services/queries";
 import { Clock } from "lucide-react";
 import Link from "next/link";
 
 const BlogPage = () => {
+  const { data: blogs, isLoading, error } = useBlogs();
+
+  if (isLoading)
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        Loading...
+      </div>
+    );
+  if (error)
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        Error loading jobs
+      </div>
+    );
+
   return (
     <main className="!pt-20 min-h-dvh">
       <section className="grid gap-3 md:gap-7 myContainer">
@@ -12,27 +30,37 @@ const BlogPage = () => {
           and hiring insights.
         </p>
       </section>
-      <section className="myContainer !pt-0 grid md:grid-cols-2 gap-x-4 gap-y-7 lg:grid-cols-3 max-w-[1440px] mx-auto">
-        {Array.from({ length: 6 }, (_, i) => (
-          <div key={i} className="border border-gray-300 rounded-2xl p-2">
-            <Link href={`/blog/${"5 Proven Strategies to Attract Top Talent in 2024"}`}>
-              <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
-              <h4 className="text-base md:text-lg font-aeoBold mt-2">
-                5 Proven Strategies to Attract Top Talent in 2024
-              </h4>
-              <p className="line-clamp-2 mt-2">
-                Discover actionable tips to stand out in a competitive market
-                and secure the best candidates for your team.
-              </p>
-              <div className="myFlex gap-1 opacity-85 mt-4">
-                <Clock size={18} />
-                <p className="text-sm">3 mins reading time</p>
-              </div>
-            </Link>
-          </div>
-        ))}
-      </section>
-      <p className="text-center font-aeoReg mt-7 mb-20 underline">Load More</p>
+
+      {blogs?.data && blogs?.data.length > 0 ? (
+        <section className="myContainer !pt-0 grid md:grid-cols-2 gap-x-4 gap-y-7 lg:grid-cols-3 max-w-[1440px] mx-auto">
+          {blogs?.data.map((blog, i) => (
+            <div key={i} className="border border-gray-300 rounded-2xl p-2">
+              <Link
+                href={`/blog/${"5 Proven Strategies to Attract Top Talent in 2024"}`}
+              >
+                <div className="bg-gray-100 h-[150px] w-full rounded-[8px]" />
+                <h4 className="text-base md:text-lg font-aeoBold mt-2">
+                  5 Proven Strategies to Attract Top Talent in 2024
+                </h4>
+                <p className="line-clamp-2 mt-2">
+                  Discover actionable tips to stand out in a competitive market
+                  and secure the best candidates for your team.
+                </p>
+                <div className="myFlex gap-1 opacity-85 mt-4">
+                  <Clock size={18} />
+                  <p className="text-sm">3 mins reading time</p>
+                </div>
+              </Link>
+            </div>
+          ))}
+        </section>
+      ) : (
+        <div className="min-h-[300px] flex items-center justify-center">
+          No Blog Available
+        </div>
+      )}
+
+      {/* <p className="text-center font-aeoReg mt-7 mb-20 underline">Load More</p> */}
       <CTA
         isBtn={false}
         heading="Stay Updated with the Latest Insights"
